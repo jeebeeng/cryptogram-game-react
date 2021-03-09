2aa0d32446c605ec88b04ca196aff7dca29aae37
John Bang <jeebeeng@gmail.com>
2021-03-09 16:04:31 -0500

    change game from class to methods

--- src/game.js
+++ src/game.js
@@ -1,96 +1,84 @@
-class Game {
-  constructor(quote) {
-    this.author = quote.author;
-    this.quote = quote.content;
-    this.letterMap = this.#randomize();
-    this.scrambledQuote = this.#getScrambledQuote();
-  }
+export const randomize = (quote) => {
+  let map = new Map();
+  let set = new Set();
+  let usedLetters = [];
+  const letters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  const punctuations = ['.', ',', ':', '!', '?', ';', ':', '-', "'"];
 
-  #randomize = () => {
-    let map = new Map();
-    let set = new Set();
-    let usedLetters = [];
-    const letters = [
-      'A',
-      'B',
-      'C',
-      'D',
-      'E',
-      'F',
-      'G',
-      'H',
-      'I',
-      'J',
-      'K',
-      'L',
-      'M',
-      'N',
-      'O',
-      'P',
-      'Q',
-      'R',
-      'S',
-      'T',
-      'U',
-      'V',
-      'W',
-      'X',
-      'Y',
-      'Z',
-    ];
-    const punctuations = ['.', ',', ':', '!', '?', ';', ':', '-', "'"];
+  [...quote.toUpperCase()].forEach((char) => {
+    set.add(char);
+  });
 
-    [...this.quote.toUpperCase()].forEach((char) => {
-      set.add(char);
+  [...set]
+    .filter((char) => !punctuations.includes(char) && char !== ' ')
+    .forEach((char) => {
+      char = char.toUpperCase();
+      const filteredLetters = letters.filter(
+        (letter) => !usedLetters.includes(letter) && letter !== char
+      );
+      let randomLetter = filteredLetters[getRandomInt(filteredLetters.length)];
+      usedLetters.push(randomLetter);
+      map.set(char, { assigned: randomLetter, guess: '' });
     });
 
-    [...set]
-      .filter((char) => !punctuations.includes(char) && char !== ' ')
-      .forEach((char) => {
-        char = char.toUpperCase();
-        const filteredLetters = letters.filter(
-          (letter) => !usedLetters.includes(letter) && letter !== char
-        );
-        let randomLetter =
-          filteredLetters[this.#getRandomInt(filteredLetters.length)];
-        usedLetters.push(randomLetter);
-        map.set(char, { assigned: randomLetter, guess: '' });
-      });
+  return map;
+};
 
-    return map;
-  };
-
-  #getScrambledQuote = () => {
-    const scrambledQuote = this.quote
-      .toUpperCase()
-      .split('')
-      .map((letter) => {
-        if (this.letterMap.has(letter)) {
-          return this.letterMap.get(letter).assigned;
-        }
-        return letter;
-      })
-      .join('');
-
-    return scrambledQuote;
-  };
-
-  /**
-   * Check if each of the guesses match the original letter (key)
-   * @return whether each of the guesses match the original letter (key)
-   */
-  checkGuessedLetters = () => {
-    [...this.letterMap.keys()].forEach((key) => {
-      if (this.letterMap.get(key).guess !== key) {
-        return false;
+export const getScrambledQuote = (quote, letterMap) => {
+  const scrambledQuote = quote
+    .toUpperCase()
+    .split('')
+    .map((letter) => {
+      if (letterMap.has(letter)) {
+        return letterMap.get(letter).assigned;
       }
-      return true;
-    });
-  };
+      return letter;
+    })
+    .join('');
 
-  #getRandomInt = (max) => {
-    return Math.floor(Math.random() * Math.floor(max));
-  };
-}
+  return scrambledQuote;
+};
 
-export default Game;
+/**
+ * Check if each of the guesses match the original letter (key)
+ * @return whether each of the guesses match the original letter (key)
+ */
+export const checkGuessedLetters = (letterMap) => {
+  [...letterMap.keys()].forEach((key) => {
+    if (letterMap.get(key).guess !== key) {
+      return false;
+    }
+    return true;
+  });
+};
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
